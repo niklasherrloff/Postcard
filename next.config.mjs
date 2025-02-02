09cf27aaaa5bd49540db08882187f784bee36128
niklasgolf <niklas.herrloff@gmail.com>
2025-02-02 10:49:36 +0100

    First commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
+import nextPwa from 'next-pwa';
+
+const withPWA = nextPwa({
+  dest: 'public',
+});
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
